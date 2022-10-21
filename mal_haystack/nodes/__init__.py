2c4ae0a3f16579d4c61d41435e20fcb1c7883046
Brian McClune <brian.mcclune@unnpp.gov>
2022-10-21 15:59:45 -0400

    Implement QAExtractor node

--- mal_haystack/nodes/__init__.py
+++ mal_haystack/nodes/__init__.py
@@ -1,7 +1,13 @@
 """MAL Haystack custom pipeline nodes."""
 
 from .dataframe_converter import DataFrameConverter
+from .qa_extractor import QAExtractor
 from .zip_dataframer import ZipDataFramer
 from .zip_lister import ZipLister
 
-__all__ = ['DataFrameConverter', 'ZipDataFramer', 'ZipLister']
+__all__ = [
+    'DataFrameConverter',
+    'QAExtractor',
+    'ZipDataFramer',
+    'ZipLister',
+]
