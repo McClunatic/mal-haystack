394f23ccbfc4e20c3014d08f4e482775b69c5478
Brian McClune <brian.mcclune@unnpp.gov>
2022-10-23 11:34:40 -0400

    Add DataFramer class (for unzipped CSV files)

--- mal_haystack/nodes/__init__.py
+++ mal_haystack/nodes/__init__.py
@@ -1,12 +1,14 @@
 """MAL Haystack custom pipeline nodes."""
 
 from .dataframe_converter import DataFrameConverter
+from .dataframer import DataFramer
 from .qa_extractor import QAExtractor
 from .zip_dataframer import ZipDataFramer
 from .zip_lister import ZipLister
 
 __all__ = [
     'DataFrameConverter',
+    'DataFramer',
     'QAExtractor',
     'ZipDataFramer',
     'ZipLister',
